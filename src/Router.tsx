d8201f10d1766dbc6e02599b2e86fcee795191a3
Felipe <75274860+luizfelipe63@users.noreply.github.com>
2022-12-22 16:21:16 -0300

    Modified: Delete import DefultTheme

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,4 @@
 import {Routes, Route} from 'react-router-dom'
-import { DefaultLayuot } from './layout/DefaultLayout'
 import { Checkout } from './pages/checkout'
 import { Home } from './pages/home'
 import { Success } from './pages/success'
@@ -7,11 +6,9 @@ import { Success } from './pages/success'
 export function Router(){
   return(
     <Routes>
-      <Route path='/' element={<DefaultLayuot/>}>
       <Route path='/' element={<Home/>}/>
       <Route path='/checkout' element={<Checkout/>}/>
       <Route path='/success' element={<Success/>}/>
-      </Route>
     </Routes>
   )
   
