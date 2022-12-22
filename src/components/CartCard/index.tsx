00641ee1e27c3cd19f1ef9cc6b640dcf930677ab
Felipe <75274860+luizfelipe63@users.noreply.github.com>
2022-12-22 16:28:29 -0300

    Name folder

---
+++ src/components/CartCard/index.tsx
@@ -0,0 +1,24 @@
+import { Trash } from 'phosphor-react'
+import Coffe from '../../assets/images/Coffee.svg'
+import { ActionsCar, Details, InfoCoffe } from './styles'
+
+export function CoffeSelected(){
+  return(
+    <InfoCoffe>
+      <Details>
+        <img src={Coffe} alt="" />
+        <div>
+          <span>Expresso Tradicional</span>
+          <ActionsCar>
+            <input type="number" min={1}/>
+            <button>
+              <Trash size={16}/>
+              REMOVER
+            </button>
+          </ActionsCar>
+        </div>
+      </Details>
+      <strong>R$ 9,90</strong>
+    </InfoCoffe>
+  )
+}
